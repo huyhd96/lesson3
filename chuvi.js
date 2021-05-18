8a86bc10763513eb087e20d5cc1342b6b30ddadd
huyhd96 <huychelsea0@gmail.com>
2021-05-18 09:14:33 -0400

    add lesson2

---
+++ chuvi.js
@@ -0,0 +1,7 @@
+const pi = 3.14
+let R = +prompt("Nhập vào bán kính hình tròn(cm)")
+let C = 2 * R * pi
+let S = R * R * pi
+
+document.write("Chu vi hình tròn là:" + C + "cm" + "<br>")
+document.write("Diện tích hình tròn là:" + S + "cm2")
