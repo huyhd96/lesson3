e766bf87d184d83b7beb99740485e6e729f88623
huyhd96 <huychelsea0@gmail.com>
2021-05-18 08:47:18 -0400

    chang value

--- caculator.js
+++ caculator.js
@@ -1,4 +1,5 @@
 let a = +prompt("nhập số tiền cần vay:")
 let b = +prompt("Nhập số năm vay:")
-let c = a + b * 69 / 100
-alert("Số tiền cần trả sau" + b + " năm " + " là " + c)
+let c = a * 69 / 100
+let d = a + c * b
+alert("Số tiền cần trả sau" + b + " năm " + " là " + d + "vnd")
